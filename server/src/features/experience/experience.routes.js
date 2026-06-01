edaacd9a8a8b0bf49343a930c7743e324972f801
Prem Raichura <premraichura7@gmail.com>
2026-06-01 15:46:01 +0530

    server experience images added

--- server/src/features/experience/experience.routes.js
+++ server/src/features/experience/experience.routes.js
@@ -8,17 +8,59 @@ import {
   deleteExperience,
 } from "./experience.controller.js";
 
+import {
+  upload,
+} from "../../config/multer.js";
+
 import { protect } from "../../shared/middleware/auth/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, createExperience);
+router.post(
+  "/",
+
+  protect,
+
+  (req, res, next) => {
+
+    req.uploadFolder =
+      "uploads/experiences";
+
+    next();
+  },
+
+  upload.array(
+    "images",
+    10
+  ),
+
+  createExperience
+);
 
 router.get("/", protect, getExperiences);
 
 router.get("/:slug", protect, getSingleExperience);
 
-router.put("/:slug", protect, updateExperience);
+router.put(
+  "/:slug",
+
+  protect,
+
+  (req, res, next) => {
+
+    req.uploadFolder =
+      "uploads/experiences";
+
+    next();
+  },
+
+  upload.array(
+    "images",
+    10
+  ),
+
+  updateExperience
+);
 
 router.delete("/:slug", protect, deleteExperience);
 
